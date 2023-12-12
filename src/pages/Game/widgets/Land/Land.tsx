9fdf7a72fc7ac53bbda64c63d0536583970a263e
mobo <morgan.bohn@pc-scol.fr>
2023-12-12 17:10:00 +0100

    fix hover

--- src/pages/Game/widgets/Land/Land.tsx
+++ src/pages/Game/widgets/Land/Land.tsx
@@ -1,5 +1,5 @@
 import { OutputContainer } from 'components/OutputContainer';
-import { Suspense, useRef, useState } from 'react';
+import { Suspense, useEffect, useRef, useState } from 'react';
 import { Canvas } from '@react-three/fiber';
 import { Model101 } from './Model101';
 import { OrbitControls, PerspectiveCamera, Stage } from '@react-three/drei';
@@ -28,6 +28,19 @@ export const Land = () => {
   const [clicked4, click4] = useState(false);
   const [clicked5, click5] = useState(false);
 
+  const [hovered1, hover1] = useState(false);
+  const [hovered2, hover2] = useState(false);
+  const [hovered3, hover3] = useState(false);
+  const [hovered4, hover4] = useState(false);
+  const [hovered5, hover5] = useState(false);
+
+  useEffect(() => {
+    document.body.style.cursor =
+      hovered1 || hovered2 || hovered3 || hovered4 || hovered5
+        ? 'pointer'
+        : 'auto';
+  }, [hovered1, hovered2, hovered3, hovered4, hovered5]);
+
   return (
     <Canvas shadows dpr={[1, 2]}>
       <Suspense fallback={<Loader />}>
@@ -37,13 +50,19 @@ export const Land = () => {
           intensity={4}
           environment='dawn'
         >
-          <Model101 
-          onPointerOver={(event) => hover(true)}
-          onPointerOut={(event) => hover(false)}
+          <Model101
+            onPointerOver={(event) => hover(true)}
+            onPointerOut={(event) => hover(false)}
           />
           {!clicked1 ? (
             <Grave
-              onClick={(event) => click1(!clicked1)}
+              onClick={(event) => {
+                hover1(false);
+                click1(!clicked1);
+              }}
+              onPointerOver={(event) => hover1(true)}
+              onPointerOut={(event) => hover1(false)}
+              color={hovered1 ? 'blue' : 'white'}
               position={[3, 1.2, -2.5]}
             />
           ) : (
@@ -51,7 +70,13 @@ export const Land = () => {
           )}
           {!clicked2 ? (
             <Grave
-              onClick={(event) => click2(!clicked2)}
+              onClick={(event) => {
+                hover2(false);
+                click2(!clicked2);
+              }}
+              onPointerOver={(event) => hover2(true)}
+              onPointerOut={(event) => hover2(false)}
+              color={hovered2 ? 'blue' : 'white'}
               position={[1, 1.1, 3]}
             />
           ) : (
@@ -59,7 +84,13 @@ export const Land = () => {
           )}
           {!clicked3 ? (
             <Grave
-              onClick={(event) => click3(!clicked3)}
+              onClick={(event) => {
+                hover3(false);
+                click3(!clicked3);
+              }}
+              onPointerOver={(event) => hover3(true)}
+              onPointerOut={(event) => hover3(false)}
+              color={hovered3 ? 'blue' : 'white'}
               position={[-5, 1.1, 5]}
             />
           ) : (
@@ -67,7 +98,13 @@ export const Land = () => {
           )}
           {!clicked4 ? (
             <Grave
-              onClick={(event) => click4(!clicked4)}
+              onClick={(event) => {
+                hover4(false);
+                click4(!clicked4);
+              }}
+              onPointerOver={(event) => hover4(true)}
+              onPointerOut={(event) => hover4(false)}
+              color={hovered4 ? 'blue' : 'white'}
               position={[-6, 2, -6]}
             />
           ) : (
@@ -75,7 +112,13 @@ export const Land = () => {
           )}
           {!clicked5 ? (
             <Grave
-              onClick={(event) => click5(!clicked5)}
+              onClick={(event) => {
+                hover5(false);
+                click5(!clicked5);
+              }}
+              onPointerOver={(event) => hover5(true)}
+              onPointerOut={(event) => hover5(false)}
+              color={hovered5 ? 'blue' : 'white'}
               position={[3, 1.3, -8]}
             />
           ) : (
