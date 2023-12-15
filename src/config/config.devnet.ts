f835777d634c460af465bbae546abd17e3a4b3c4
mobo <morgan.bohn@pc-scol.fr>
2023-12-15 18:09:20 +0100

    sc wip

--- src/config/config.devnet.ts
+++ src/config/config.devnet.ts
@@ -14,3 +14,5 @@ export const sftLandsId = 'DEVSHALAN-933f80-01';
 export const sftCryptId = 'DEVSHALAN-933f80-02';
 export const sftHauntedHouseId = 'DEVSHALAN-933f80-04';
 export const dustTokenId = 'DEVDUST-d62981';
+export const contractGameAddress =
+  'erd1qqqqqqqqqqqqqpgqc4s83k0ut7c2p6v3rqlddz4wtz7djhq42krs0m3afj';
