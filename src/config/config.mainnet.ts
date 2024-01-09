6c8ec7953039b0d0301112eec48325613b266909
mobo <morgan.bohn@pc-scol.fr>
2024-01-09 18:48:40 +0100

    prepare prod conf

--- src/config/config.mainnet.ts
+++ src/config/config.mainnet.ts
@@ -6,26 +6,26 @@ export const environment = EnvironmentsEnum.mainnet;
 
 // Shadow Lands
 export const mvxApiUrl = 'https://api.multiversx.com';
-export const sftCollectionId = 'DEVSHALAN-933f80';
-export const sftLandsId = 'DEVSHALAN-933f80-01';
+export const sftCollectionId = 'xxx';
+export const sftLandsId = 'xxx-01';
 export const sftLandsNonce = 1;
-export const sftTavernId = 'DEVSHALAN-933f80-02';
+export const sftTavernId = 'xxx-02';
 export const sftTavernNonce = 2;
-export const sftBanksId = 'DEVSHALAN-933f80-04';
-export const sftBanksNonce = 4;
-export const sftHauntedHouseId = 'DEVSHALAN-933f80-05';
-export const sftHauntedHouseNonce = 5;
-export const sftCryptId = 'DEVSHALAN-933f80-06';
-export const sftCryptNonce = 6;
-export const sftLaboId = 'DEVSHALAN-933f80-07';
-export const sftLaboNonce = 7;
+export const sftBanksId = 'xxx-03';
+export const sftBanksNonce = 3;
+export const sftHauntedHouseId = 'xxx-04';
+export const sftHauntedHouseNonce = 4;
+export const sftCryptId = 'xxx-05';
+export const sftCryptNonce = 5;
+export const sftLaboId = 'xxx-06';
+export const sftLaboNonce = 6;
 export const dustTokenId = 'DUST-e7e490';
 export const contractGameAddress =
-  'erd1qqqqqqqqqqqqqpgqvfh5j05pw23upxz4hmh6fdsrcmvfjjd22krsxc4hy2';
+  'xxx';
 export const contractMarketAddress =
-  'erd1qqqqqqqqqqqqqpgqsp7wpnxv8lpn6hg4fhhp3vqkwmst5ljm2krs2fxjrk';
-export const totalYield = 60;
-export const totalProducted = 10000;
+  'xxx';
+export const totalYield = 30;
+export const totalProducted = 5000;
 
 // Price
 export const priceLand = 150;
