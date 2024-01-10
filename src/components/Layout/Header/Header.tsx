abbf137cbd04e6cdb52f2631061060ccd1ce8f56
mobo <morgan.bohn@pc-scol.fr>
2024-01-10 13:33:39 +0100

    add erd address in header

--- src/components/Layout/Header/Header.tsx
+++ src/components/Layout/Header/Header.tsx
@@ -2,11 +2,12 @@ import { Button } from 'components/Button';
 import { MxLink } from 'components/MxLink';
 import { environment } from 'config';
 import { logout } from 'helpers';
-import { useGetIsLoggedIn } from 'hooks';
+import { useGetAccountInfo, useGetIsLoggedIn } from 'hooks';
 import { RouteNamesEnum } from 'localConstants';
 
 export const Header = () => {
   const isLoggedIn = useGetIsLoggedIn();
+  const { address, account } = useGetAccountInfo();
 
   const handleLogout = () => {
     sessionStorage.clear();
@@ -30,12 +31,19 @@ export const Header = () => {
           </div>
 
           {isLoggedIn ? (
-            <Button
-              onClick={handleLogout}
-              className='inline-block rounded-lg px-3 py-2 text-center hover:no-underline my-0 text-gray-400 hover:text-gray-600 hover:bg-slate-100 mx-0'
-            >
-              Close
-            </Button>
+            <>
+              <span className='text-gray-400'>
+                {address.substring(0, 6) +
+                  '...' +
+                  address.substring(address.length - 6)}
+              </span>
+              <Button
+                onClick={handleLogout}
+                className='inline-block rounded-lg px-3 py-2 text-center hover:no-underline my-0 text-gray-400 hover:text-gray-600 hover:bg-slate-100 mx-0'
+              >
+                Close
+              </Button>
+            </>
           ) : (
             <MxLink to={RouteNamesEnum.unlock}>Connect</MxLink>
           )}
