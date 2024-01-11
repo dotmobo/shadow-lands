2f5f2f7fcae7b3d8f5ac7a24f6f5f77281ff082c
mobo <morgan.bohn@pc-scol.fr>
2024-01-11 09:57:01 +0100

    fix help

--- src/pages/Help/Help.tsx
+++ src/pages/Help/Help.tsx
@@ -8,11 +8,11 @@ export const Help = () => {
   return (
     <AuthRedirectWrapper requireAuth={true}>
       <PageWrapper>
-        <div className='flex flex-col items-center h-full w-full bg-slate-900 text-slate-400'>
+        <div className='flex flex-col items-center h-full w-full bg-slate-900 text-slate-400 mb-2'>
           <h1 className='text-4xl sm:text-4xl font-bold mt-4 mb-8'>
             How to play ?
           </h1>
-          <ul className='list-disc ml-8 mb-8'>
+          <ul className='list-disc ml-8 mb-8 mr-2'>
             <li>Use the market to buy one land</li>
             <li>Place the land and earn some $DUST</li>
             <li>Buy some other buildings like one tavern or one crypt</li>
@@ -21,11 +21,34 @@ export const Help = () => {
               Claim your $DUST and become the biggest $DUST holder to receive
               some gifts!
             </li>
+            <li>Check the leaderboard to view your position.</li>
             <li>
               <b>Warning</b>: you can only place one building of each type on
               the map!
             </li>
           </ul>
+          <h2 className='text-2xl sm:text-2xl font-bold mb-8'>Actions</h2>
+          <ul className='list-disc ml-8 mb-8 mr-2'>
+            <li>
+              <b>Add the Land</b>: Stake a land from your wallet if it's not
+              already done.
+            </li>
+            <li>
+              <b>Destroy the Land</b>: Unstake all SFTs from the game. Warning,
+              non-claimed rewards will be lost!
+            </li>
+            <li>
+              <b>Claim</b>: Claim your current rewards from the farm.
+            </li>
+            <li>
+              <b>Buy 1 XXX</b>: Buy a land or a building like a tavern in $DUST.
+            </li>
+            <li>
+              <b>External Market</b>: Buy a land or a building like a tavern in
+              EGLD.
+            </li>
+          </ul>
+
           <MxLink to={RouteNamesEnum.game}>
             <FontAwesomeIcon
               icon={faCircleArrowLeft}
