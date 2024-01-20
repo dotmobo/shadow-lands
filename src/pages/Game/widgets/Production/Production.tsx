5e25498817061b8c4fd10c325ecfd8a81eec243a
Morgan <morgan.bohn@pc-scol.fr>
2024-01-20 13:25:19 +0100

    external farm

--- src/pages/Game/widgets/Production/Production.tsx
+++ src/pages/Game/widgets/Production/Production.tsx
@@ -121,6 +121,17 @@ export const Production = ({ sfts, rewardPerDay }) => {
           <FontAwesomeIcon icon={faTruckLoading} className='mr-1' />
           Claim
         </Button>
+        <Button
+          className='ml-8 inline-block rounded-lg px-3 py-2 text-center hover:no-underline my-0 bg-purple-600 text-white hover:bg-purple-700 mr-0 disabled:bg-gray-200 disabled:text-black disabled:cursor-not-allowed'
+          onClick={() =>
+            window.open(
+              'https://swap.onedex.app/farm'
+            )
+          }
+        >
+          <FontAwesomeIcon icon={faStore} className='mr-1' />
+          External Farm
+        </Button>
       </div>
     </OutputContainer>
   );
