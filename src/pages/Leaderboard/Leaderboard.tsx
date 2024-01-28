6fc4c88fe625c26c8f2ccd4e911dcac9c815a4a6
Morgan <morgan.bohn@pc-scol.fr>
2024-01-28 17:40:26 +0100

    leaderboard basé sur le claim

--- src/pages/Leaderboard/Leaderboard.tsx
+++ src/pages/Leaderboard/Leaderboard.tsx
@@ -5,7 +5,6 @@ import axios from 'axios';
 import { FormatAmount, Loader, MxLink } from 'components';
 import {
   contractGameAddress,
-  dustTokenId,
   ignoredAddresses,
   mvxApiUrl,
   mvxExplorerUrl
@@ -17,55 +16,54 @@ import { AuthRedirectWrapper, PageWrapper } from 'wrappers';
 export const Leaderboard = () => {
   const { hasPendingTransactions } = useGetPendingTransactions();
   const [leaderboard, setLeaderboard] = useState<any>();
-  const [claimers, setClaimers] = useState<string[]>();
 
   useEffect(() => {
-    // Use [] as second argument in useEffect for not rendering each time
-    axios
-      .get<any>(`${mvxApiUrl}/tokens/${dustTokenId}/accounts?size=10000`)
-      .then((response) => {
-        setLeaderboard(
-          response.data.filter(
-            (item: any) =>
-              ignoredAddresses.includes(item.address) === false &&
-              item.address.startsWith('erd1qqqqqq') === false
-          )
-        );
-      })
-      .catch((error) => {
-        if (error.response.status === 404) {
-          setLeaderboard([]);
-        }
-      });
-  }, [hasPendingTransactions]);
-
-  useEffect(() => {
+    const today = new Date();
+    today.setDate(1); // Définir le jour actuel au 1er jour du mois
     const previousMonth = Math.floor(
-      new Date().setMonth(new Date().getMonth() - 1) / 1000
+      today.setMonth(today.getMonth() - 1) / 1000
     );
 
     // Use [] as second argument in useEffect for not rendering each time
     axios
       .get<any>(
-        `${mvxApiUrl}/accounts/${contractGameAddress}/transactions?size=10000&status=success&function=claim&after=${previousMonth}&fields=sender`
+        `${mvxApiUrl}/accounts/${contractGameAddress}/transfers?sender=${contractGameAddress}&size=10000&status=success&function=ESDTTransfer&after=${previousMonth}`
       )
       .then((response) => {
-        console.log('response', response.data);
-        setClaimers(
-          response.data
-            .filter(
-              (item: any) =>
-                ignoredAddresses.includes(item.sender) === false &&
-                item.sender.startsWith('erd1qqqqqq') === false
-            )
-            .map((item: any) => item.sender)
+        const playersData = response.data.filter(
+          (item: any) =>
+            ignoredAddresses.includes(item.receiver) === false &&
+            item.receiver.startsWith('erd1qqqqqq') === false
         );
-        console.log('claimers', claimers);
+
+        const calculateTotalForReceiver = (receiver: string) => {
+          return playersData
+            .filter((transaction: any) => transaction.receiver === receiver)
+            .map(
+              (transaction: any) =>
+                transaction.action.arguments.transfers[0].value
+            )
+            .map((value: string) => parseInt(value))
+            .reduce((acc: number, value: number) => acc + value, 0);
+        };
+
+        const uniqueReceivers = [
+          ...new Set(
+            playersData.map((transaction: any) => transaction.receiver)
+          )
+        ];
+
+        const resultList = uniqueReceivers
+          .map((receiver: any) => ({
+            address: receiver,
+            balance: calculateTotalForReceiver(receiver).toString()
+          }))
+          .sort((a: any, b: any) => parseInt(b.balance) - parseInt(a.balance));
+
+        setLeaderboard(resultList);
       })
-      .catch((error) => {
-        if (error.response.status === 404) {
-          setClaimers([]);
-        }
+      .catch((_error) => {
+        setLeaderboard([]);
       });
   }, [hasPendingTransactions]);
 
@@ -74,57 +72,51 @@ export const Leaderboard = () => {
       <PageWrapper>
         <div className='flex flex-col items-center h-full w-full bg-slate-900 text-slate-400'>
           <h1 className='text-4xl sm:text-4xl font-bold mt-4 mb-2 ml-2 mr-2'>
-            Leaderboard Top 10
+            Leaderboard {new Date().toLocaleString('en-US', { month: 'long' })} Top 10
           </h1>
           <span className='ml-2 mr-2 mb-8 italic'>
             To stay on the leaderboard, make sure to claim rewards within the
             last month.
           </span>
-          {leaderboard === undefined && claimers === undefined && (
+          {leaderboard === undefined && (
             <div className='flex'>
               <Loader />
             </div>
           )}
-          {claimers !== undefined &&
-            claimers.length > 0 &&
-            leaderboard !== undefined &&
-            leaderboard.length > 0 && (
-              <ul className='list-decimal ml-8 mb-8'>
-                {leaderboard
-                  .filter((item: any) => claimers.includes(item.address))
-                  .slice(0, 10)
-                  .map((item: any, index: number) => (
-                    <li key={index} className='mb-2'>
-                      <span className='flex flex-items-center'>
-                        <a
-                          target='_blank'
-                          className='text-slate-400 hover:text-slate-100'
-                          href={`${mvxExplorerUrl}/accounts/${item.address}`}
-                        >
-                          {item.address.substring(0, 6) +
-                            '...' +
-                            item.address.substring(item.address.length - 6)}
-                          &nbsp;:&nbsp;
-                        </a>
-                        <FormatAmount
-                          value={item.balance ?? 0}
-                          showLabel={false}
-                          egldLabel='$DUST'
-                          digits={0}
-                          data-testid='balance'
-                        />
-                        <span>
-                          <img
-                            src='/dust-logo.png'
-                            alt='Dust'
-                            className='ml-1 w-5'
-                          />
-                        </span>
-                      </span>
-                    </li>
-                  ))}
-              </ul>
-            )}
+          {leaderboard !== undefined && leaderboard.length > 0 && (
+            <ul className='list-decimal ml-8 mb-8'>
+              {leaderboard.slice(0, 10).map((item: any, index: number) => (
+                <li key={index} className='mb-2'>
+                  <span className='flex flex-items-center'>
+                    <a
+                      target='_blank'
+                      className='text-slate-400 hover:text-slate-100'
+                      href={`${mvxExplorerUrl}/accounts/${item.address}`}
+                    >
+                      {item.address.substring(0, 6) +
+                        '...' +
+                        item.address.substring(item.address.length - 6)}
+                      &nbsp;:&nbsp;
+                    </a>
+                    <FormatAmount
+                      value={item.balance ?? 0}
+                      showLabel={false}
+                      egldLabel='$DUST'
+                      digits={0}
+                      data-testid='balance'
+                    />
+                    <span>
+                      <img
+                        src='/dust-logo.png'
+                        alt='Dust'
+                        className='ml-1 w-5'
+                      />
+                    </span>
+                  </span>
+                </li>
+              ))}
+            </ul>
+          )}
           <span className='mb-2'>
             <MxLink to={RouteNamesEnum.game}>
               <FontAwesomeIcon
