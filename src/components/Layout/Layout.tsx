5d59d64bff74bb3492e4094235eafada8db046e3
mobo <morgan.bohn@pc-scol.fr>
2024-02-05 15:18:53 +0100

    improve style 2

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -11,7 +11,7 @@ export const Layout = ({ children }: PropsWithChildren) => {
   return (
     <div className='flex min-h-screen flex-col bg-slate-900'>
       <Header />
-      <main className='flex flex-grow items-stretch justify-center p-0'>
+      <main className='flex flex-grow items-stretch justify-center p-1'>
         <AuthenticatedRoutesWrapper
           routes={routes}
           unlockRoute={`${RouteNamesEnum.unlock}${search}`}
