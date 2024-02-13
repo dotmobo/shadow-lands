e47bbc95a76905ec3976c164b5a4a6d2511d7101
mobo <morgan.bohn@pc-scol.fr>
2024-02-13 11:31:16 +0100

    fix temporary quest

--- src/pages/Help/Help.tsx
+++ src/pages/Help/Help.tsx
@@ -20,13 +20,11 @@ import {
   sftBanksNonce,
   sftCryptNonce,
   sftCryptR1Nonce,
-  sftCryptR2Nonce,
   sftHauntedHouseNonce,
   sftHauntedHouseR1Nonce,
   sftHauntedHouseR2Nonce,
   sftLaboNonce,
   sftLaboR1Nonce,
-  sftLaboR2Nonce,
   sftLandsNonce,
   sftTavernNonce,
   sftTavernR1Nonce,
@@ -352,40 +350,6 @@ export const Help = () => {
                     className='ms-2 text-sm font-medium text-gray-900 dark:text-gray-300'
                   >
                     Upgrade the Haunted House to level +2.
-                  </label>
-                </li>
-                <li className='flex items-center mb-4'>
-                  <input
-                    id='quest15'
-                    type='checkbox'
-                    checked={
-                      sfts.filter((x) => x === sftCryptR2Nonce).length > 0
-                    }
-                    readOnly={true}
-                    className='w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600'
-                  />
-                  <label
-                    htmlFor='quest15'
-                    className='ms-2 text-sm font-medium text-gray-900 dark:text-gray-300'
-                  >
-                    Upgrade the Crypt to level +2.
-                  </label>
-                </li>
-                <li className='flex items-center mb-4'>
-                  <input
-                    id='quest16'
-                    type='checkbox'
-                    checked={
-                      sfts.filter((x) => x === sftLaboR2Nonce).length > 0
-                    }
-                    readOnly={true}
-                    className='w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600'
-                  />
-                  <label
-                    htmlFor='quest16'
-                    className='ms-2 text-sm font-medium text-gray-900 dark:text-gray-300'
-                  >
-                    Upgrade the Labo to level +2.
                   </label>
                 </li>
               </ul>
