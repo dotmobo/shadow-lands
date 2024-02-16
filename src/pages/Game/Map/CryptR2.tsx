606a955f35446d7f8d624f8922cdbb199d180378
mobo <morgan.bohn@pc-scol.fr>
2024-02-16 19:49:33 +0100

    crypt+2

--- src/pages/Game/Map/CryptR2.tsx
+++ src/pages/Game/Map/CryptR2.tsx
@@ -7,7 +7,7 @@ import { useGLTF, useAnimations } from '@react-three/drei';
 
 export function CryptR2({ color, ...props }: any) {
   const group = useRef();
-  const { nodes, materials, animations } = useGLTF('/game/Crypt+1.glb');
+  const { nodes, materials, animations } = useGLTF('/game/Crypt+2.glb');
   const { actions } = useAnimations(animations, group);
 
   const position = [1.8028, 1.378, -5.3541];
@@ -26,93 +26,14 @@ export function CryptR2({ color, ...props }: any) {
           intensity={Math.PI * 2}
         />
       )}
-      <group name='Scene'>
-        <group name='Crypte001'>
-          <mesh
-            name='Plane002'
-            castShadow
-            receiveShadow
-            geometry={nodes.Plane002.geometry}
-            material={materials.Base}
-          >
-            {color !== undefined && (
-              <meshStandardMaterial color={color} transparent opacity={0.2} />
-            )}
-          </mesh>
-          <mesh
-            name='Plane002_1'
-            castShadow
-            receiveShadow
-            geometry={nodes.Plane002_1.geometry}
-            material={materials.Etage}
-          >
-            {color !== undefined && (
-              <meshStandardMaterial color={color} transparent opacity={0.2} />
-            )}
-          </mesh>
-          <mesh
-            name='Plane002_2'
-            castShadow
-            receiveShadow
-            geometry={nodes.Plane002_2.geometry}
-            material={materials.Colonnes}
-          >
-            {color !== undefined && (
-              <meshStandardMaterial color={color} transparent opacity={0.2} />
-            )}
-          </mesh>
-          <mesh
-            name='Plane002_3'
-            castShadow
-            receiveShadow
-            geometry={nodes.Plane002_3.geometry}
-            material={materials.Toit}
-          >
-            {color !== undefined && (
-              <meshStandardMaterial color={color} transparent opacity={0.2} />
-            )}
-          </mesh>
-          <mesh
-            name='Plane002_4'
-            castShadow
-            receiveShadow
-            geometry={nodes.Plane002_4.geometry}
-            material={materials.Vitraux}
-          >
-            {color !== undefined && (
-              <meshStandardMaterial color={color} transparent opacity={0.2} />
-            )}
-          </mesh>
-          <mesh
-            name='Plane002_5'
-            castShadow
-            receiveShadow
-            geometry={nodes.Plane002_5.geometry}
-            material={materials.Vitres}
-          >
-            {color !== undefined && (
-              <meshStandardMaterial color={color} transparent opacity={0.2} />
-            )}
-          </mesh>
-          <mesh
-            name='Plane002_6'
-            castShadow
-            receiveShadow
-            geometry={nodes.Plane002_6.geometry}
-            material={materials.Porte}
-          >
-            {color !== undefined && (
-              <meshStandardMaterial color={color} transparent opacity={0.2} />
-            )}
-          </mesh>
-        </group>
+      <group name="Scene">
         <group
-          name='Armature001'
+          name="Armature001"
           position={[1.629, 3.762, -3.95]}
           rotation={[-2.471, -1.35, -2.447]}
         >
           <skinnedMesh
-            name='Cube002'
+            name="Cube002"
             geometry={nodes.Cube002.geometry}
             material={materials.Corbeau}
             skeleton={nodes.Cube002.skeleton}
@@ -123,9 +44,95 @@ export function CryptR2({ color, ...props }: any) {
           </skinnedMesh>
           <primitive object={nodes.Bone} />
         </group>
+        <group name="Crypt+2">
+          <mesh
+            name="Plane004"
+            castShadow
+            receiveShadow
+            geometry={nodes.Plane004.geometry}
+            material={materials["Ardoises.001"]}
+          >
+            {color !== undefined && (
+              <meshStandardMaterial color={color} transparent opacity={0.2} />
+            )}
+          </mesh>
+          <mesh
+            name="Plane004_1"
+            castShadow
+            receiveShadow
+            geometry={nodes.Plane004_1.geometry}
+            material={materials.Charpente}
+          >
+            {color !== undefined && (
+              <meshStandardMaterial color={color} transparent opacity={0.2} />
+            )}
+          </mesh>
+          <mesh
+            name="Plane004_2"
+            castShadow
+            receiveShadow
+            geometry={nodes.Plane004_2.geometry}
+            material={materials["Colonnes.001"]}
+          >
+            {color !== undefined && (
+              <meshStandardMaterial color={color} transparent opacity={0.2} />
+            )}
+          </mesh>
+          <mesh
+            name="Plane004_3"
+            castShadow
+            receiveShadow
+            geometry={nodes.Plane004_3.geometry}
+            material={materials["Base.001"]}
+          >
+            {color !== undefined && (
+              <meshStandardMaterial color={color} transparent opacity={0.2} />
+            )}
+          </mesh>
+          <mesh
+            name="Plane004_4"
+            castShadow
+            receiveShadow
+            geometry={nodes.Plane004_4.geometry}
+            material={materials["Etage.001"]}
+          >
+            {color !== undefined && (
+              <meshStandardMaterial color={color} transparent opacity={0.2} />
+            )}
+          </mesh>
+          <mesh
+            name="Plane004_5"
+            castShadow
+            receiveShadow
+            geometry={nodes.Plane004_5.geometry}
+            material={materials["Vitres.001"]}
+          >
+            {color !== undefined && (
+              <meshStandardMaterial color={color} transparent opacity={0.2} />
+            )}
+          </mesh>
+          <mesh
+            name="Plane004_6"
+            castShadow
+            receiveShadow
+            geometry={nodes.Plane004_6.geometry}
+            material={materials["Vitraux.001"]}
+          >
+            {color !== undefined && (
+              <meshStandardMaterial color={color} transparent opacity={0.2} />
+            )}
+          </mesh>
+          <mesh
+            name="Plane004_7"
+            castShadow
+            receiveShadow
+            geometry={nodes.Plane004_7.geometry}
+            material={materials["Porte.001"]}
+          />
+        </group>
       </group>
     </group>
   );
 }
 
-useGLTF.preload('/game/Crypt+1.glb');
+useGLTF.preload('/game/Crypt+2.glb');
